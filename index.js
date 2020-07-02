9d042e6e092de2dda3dee391902f5ae1e0f696f9
Ben Michel <ben@obensource.com>
2020-07-02 15:14:56 -0700

    fix: exports

--- index.js
+++ index.js
@@ -5,9 +5,13 @@ const { tempFolderCheck, rm } = require('./utilities.js')
 const arguments = process.argv.filter((argument, i) => i > 1)
 tempFolderCheck()
 
-if (arguments.length === 0) {
-  console.log(`rm-safe: no arguments were provided\n`)
-  return process.exit(0)
-} else {
-  rm(arguments)
+const rmSafe = (arguments) => {
+  if (arguments.length === 0) {
+    console.log(`rm-safe: no arguments were provided\n`)
+    return process.exit(0)
+  } else {
+    rm(arguments)
+  }
 }
+
+module.exports = rmSafe(arguments)
